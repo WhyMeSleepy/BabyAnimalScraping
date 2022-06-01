287ca4f1714c1e820cacb1ba1354f375846c46e4
KCINlnwza007 <104412827+WhyMeSleepy@users.noreply.github.com>
2022-06-01 16:30:14 +0700

    lottery

--- jobs_scrap.py
+++ jobs_scrap.py
@@ -2,11 +2,12 @@ from bs4 import BeautifulSoup
 import requests
 
 def data_jobs(page):
+    global file_num
     html_job = requests.get(f'https://th.jobsdb.com/th/th/search-jobs/python/{str(page)}').text
     soup = BeautifulSoup(html_job,'lxml')
     jobs = soup.find_all('div',class_='sx2jih0 zcydq89e zcydq88e zcydq872 zcydq87e')
 
-    for i,job in enumerate(jobs):
+    for job in jobs:
         job_position = job.find('span',class_='sx2jih0').text
         try:
             company_name = job.find('span',class_='sx2jih0 zcydq84u _18qlyvc0 _18qlyvc1x _18qlyvc1 _18qlyvca').text
@@ -16,7 +17,7 @@ def data_jobs(page):
         salary = job.find_all('span',class_='sx2jih0 zcydq84u _18qlyvc0 _18qlyvc1x _18qlyvc3 _18qlyvc7')[-1].text
         skills = job.find_all('li',class_='sx2jih0 zcydq86i')
 
-        with open(f'jobsPosts/{page}{i}.txt','w', encoding='utf-8') as f:
+        with open(f'jobsPosts/{file_num}.txt','w', encoding='utf-8') as f:
             f.write(f'Job Positions : {job_position}\n')
             f.write(f'Company : {company_name}\n')
             f.write(f'Company Location : {location}\n')
@@ -28,9 +29,10 @@ def data_jobs(page):
                 f.write('  skill  \n')
                 for skill in skills:
                     f.write(f'- {skill.text}\n')
-
+        file_num += 1
             
 if __name__ == '__main__':
+    file_num = 1
     for page in range(1,47):
         data_jobs(page)
         print(page)
--- lottery_incomplete.py
+++ lottery_incomplete.py
@@ -1,14 +1,38 @@
-# from bs4 import BeautifulSoup
-# import requests , json
-
-# html_lottery = requests.get('https://www.thairath.co.th/lottery/archive').text
-# soup = BeautifulSoup(html_lottery,'lxml')
-# all_data = json.loads(soup)
+from bs4 import BeautifulSoup
+import requests
 
 
-# lotteries = all_data.find_all('div',class_='css-1tw01wy ecgl95t26')
-# for lottery in lotteries:
-#     date = lottery.find('h2',class_='css-uppvlb efr6tej2').span.text
-#     print(date)
 
-#     print('')
+def lottery_number(page):
+    global file_num
+    if page == 1:
+        get_html = requests.get('https://news.sanook.com/lotto/archive/').text
+    else:
+        get_html = requests.get(f'https://news.sanook.com/lotto/archive/page/{page}/').text
+    
+
+    soup = BeautifulSoup(get_html,'lxml')
+    lotteries = soup.find_all('article',class_='archive--lotto')
+    for lottery in lotteries:
+        lottery_date = lottery.find('h3',class_='archive--lotto__head-lot').text
+        
+        contain_num = lottery.find('ul',class_='archive--lotto__result-list')
+        list_num = contain_num.find_all('li')
+
+        with open('lotteryNumber/{}.txt'.format(file_num),'w',encoding='utf-8') as f:
+
+            f.write('งวดที่ '+' '.join(lottery_date.split(' ')[-3:])+'\n')
+            for i in list_num:
+                award = i.find('em',class_='archive--lotto__result-txt').text
+                number_award = i.find('strong',class_='archive--lotto__result-number').text
+                f.write('{} : {}\n'.format(award,number_award.strip().replace("\n",' ')))
+        file_num += 1
+        print('file',file_num)
+    print('page',page)
+
+
+if __name__ == '__main__':
+    file_num = 1
+    for page in range(1,20):
+        lottery_number(page)
+    
