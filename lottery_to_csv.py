8579d951c759d334f946b208e483f924969e09df
KCINlnwza007 <104412827+WhyMeSleepy@users.noreply.github.com>
2022-06-01 22:25:51 +0700

    lotteryToCSV
    yay

---
+++ lottery_to_csv.py
@@ -0,0 +1,62 @@
+from bs4 import BeautifulSoup
+import requests
+import csv
+import re
+
+def lottery_number(page):
+    global file_num
+    if page == 1:
+        get_html = requests.get('https://news.sanook.com/lotto/archive/').text
+    else:
+        get_html = requests.get(f'https://news.sanook.com/lotto/archive/page/{page}/').text
+    
+    
+
+    
+
+    soup = BeautifulSoup(get_html,'lxml')
+    lotteries = soup.find_all('article',class_='archive--lotto')
+    for lottery in lotteries:
+        lottery_date = lottery.find('h3',class_='archive--lotto__head-lot').text
+        
+        contain_num = lottery.find('ul',class_='archive--lotto__result-list')
+        list_num = contain_num.find_all('li')
+
+        
+        date = (' '.join(re.split(' |-',lottery_date)[-3:]))
+        
+        
+        firstPrize = None
+        firstThree = None
+        lastThree = None
+        lastTwo = None
+        for i in list_num:
+            award = i.find('em',class_='archive--lotto__result-txt').text
+            number_award = i.find('strong',class_='archive--lotto__result-number').text
+            if 'รางวัลที่ 1' in award:
+                firstPrize = (number_award.strip().replace("\n",' '))
+            elif 'เลขหน้า 3 ตัว' in award:
+                firstThree = (number_award.strip().replace("\n",' '))
+            elif 'เลขท้าย 3 ตัว' in award:
+                lastThree = (number_award.strip().replace("\n",' '))
+            elif 'เลขท้าย 2 ตัว' in award:
+                lastTwo = (number_award.strip().replace("\n",' '))
+
+        writer.writerow([date,firstPrize,firstThree,lastThree,lastTwo])
+        
+        print(date)
+        print(firstPrize)
+        print(firstThree)
+        print(lastThree)
+        print(lastTwo)
+        
+        
+    
+
+
+if __name__ == '__main__':
+    file = open('filecsv/lottery.csv','w',encoding='utf-8')
+    writer = csv.writer(file)
+    writer.writerow(['DrawDate','FirstPrize','FirstThree DigitsPrize','LastThreeDigitsPrize','LastTwoDigitsPrize'])
+    for page in range(1,20):
+        lottery_number(page)
