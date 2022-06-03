28f4a6eeaea552c509d14aeb22705ab068e385d5
KCINlnwza007 <104412827+WhyMeSleepy@users.noreply.github.com>
2022-06-03 11:53:11 +0700

    lottoryCSV_ID

--- LotteryScrape/lottery_to_csv.py
+++ LotteryScrape/lottery_to_csv.py
@@ -3,17 +3,24 @@ import requests
 import csv
 import re
 
+def main():
+    global writer
+    global count_id
+    count_id = 368
+    file = open('filecsv/lottery.csv','w',encoding='utf-8')
+    writer = csv.writer(file)
+    writer.writerow(['id','DrawDate','FirstPrize','FirstThree DigitsPrize','LastThreeDigitsPrize','LastTwoDigitsPrize'])
+    for page in range(1,20):
+        lottery_number(page)
+
 def lottery_number(page):
     global file_num
+    global count_id
     if page == 1:
         get_html = requests.get('https://news.sanook.com/lotto/archive/').text
     else:
         get_html = requests.get(f'https://news.sanook.com/lotto/archive/page/{page}/').text
     
-    
-
-    
-
     soup = BeautifulSoup(get_html,'lxml')
     lotteries = soup.find_all('article',class_='archive--lotto')
     for lottery in lotteries:
@@ -22,9 +29,7 @@ def lottery_number(page):
         contain_num = lottery.find('ul',class_='archive--lotto__result-list')
         list_num = contain_num.find_all('li')
 
-        
         date = (' '.join(re.split(' |-',lottery_date)[-3:]))
-        
         
         firstPrize = None
         firstThree = None
@@ -42,8 +47,8 @@ def lottery_number(page):
             elif 'เลขท้าย 2 ตัว' in award:
                 lastTwo = (number_award.strip().replace("\n",' '))
 
-        writer.writerow([date,firstPrize,firstThree,lastThree,lastTwo])
-        
+        writer.writerow([count_id,date,firstPrize,firstThree,lastThree,lastTwo])
+        count_id -= 1
         print(date)
         print(firstPrize)
         print(firstThree)
@@ -55,8 +60,4 @@ def lottery_number(page):
 
 
 if __name__ == '__main__':
-    file = open('filecsv/lottery.csv','w',encoding='utf-8')
-    writer = csv.writer(file)
-    writer.writerow(['DrawDate','FirstPrize','FirstThree DigitsPrize','LastThreeDigitsPrize','LastTwoDigitsPrize'])
-    for page in range(1,20):
-        lottery_number(page)
+    main()
